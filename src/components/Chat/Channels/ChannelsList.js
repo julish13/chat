2e7393854c8cfbd7d743a3f7e399ab000ff17240
Julia <jkolupaeva@gmail.com>
2023-03-14 23:07:09 +0400

    feat: switching chaannels in ChannelsList

--- src/components/Chat/Channels/ChannelsList.js
+++ src/components/Chat/Channels/ChannelsList.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, Nav } from 'react-bootstrap';
+import { chatActions } from '@store/redux/chat';
 
 const ChannelsList = () => {
   const { channels, currentChannelId } = useSelector((state) => state.chat);
+  const dispatch = useDispatch();
+  const changeChannelHandler = useCallback((id) => {
+    dispatch(chatActions.setCurrentChannel(id));
+  }, []);
+
   return (
     <Nav as="ul" variant="pills" fill className="flex-column px-2">
       {channels.map(({ id, name }) => (
@@ -11,6 +17,8 @@ const ChannelsList = () => {
           <Button
             variant={id === currentChannelId ? 'secondary' : null}
             className="w-100 rounded-0 text-start"
+            onClick={() => changeChannelHandler(id)}
+            type="button"
           >
             <span className="me-1">#</span>
             {name}
